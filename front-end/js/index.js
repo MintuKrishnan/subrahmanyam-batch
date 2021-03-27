a6e4cbd868d6fa830a05309fa0a92dd274ad270e
Mintu Krishnan S <mintu.krish999@hotmail.com>
2021-03-27 09:17:27 +0530

    added read files

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -18,12 +18,40 @@
 
 // console.log(m, n);
 
-function outer(m) {
-  function inner() {
-    console.log(m);
-  }
-  return inner;
-}
-let aaa = outer(9);
-console.log(aaa);
-aaa();
+// function outer(m) {
+//   function inner() {
+//     console.log(m);
+//   }
+//   return inner;
+// }
+// let aaa = outer(9);
+// console.log(aaa);
+// aaa();
+
+// const square = function (m) {
+//   return m * 2;
+// };
+
+// const square = (m) => {
+//   return m * 2;
+// };
+// const square = (m) => {
+//   return m * 2;
+// };
+
+// const square = (m) => m * 2; // one liner implicit return
+
+// const square = (
+//   m //implicit return
+// ) => m * 2;
+
+arr = [1, 2, 3, 4, 5, 6, 6, 7];
+
+const arr2 = arr.filter((el) => el > 5);
+console.log(arr2);
+
+arr2.forEach((el) => console.log(el));
+
+const arr3 = arr2.map((el) => el);
+
+console.log(arr3);
---
+++ python_lectures/40.Reading/read.py
@@ -0,0 +1,33 @@
+# main statement
+'''
+if __name__ == "__main__":
+'''
+
+# read a matrix
+'''
+r, c = list(map(int, input().rstrip().split()))
+matrix = []
+for _ in range(r):
+    arr = list(map(int, input().rstrip().split()))
+    matrix.append(arr)
+print(matrix)
+'''
+
+# read a string
+'''
+str = input()
+'''
+
+# read 2 list
+'''
+m, n = list(map(int, input().rstrip().split()))
+
+l1 = list(map(int, input().rstrip().split()))
+l2 = list(map(int, input().rstrip().split()))
+'''
+
+# read 1 list
+'''
+n = int(input())
+arr = list(map(int, input().rstrip().split()))
+'''
