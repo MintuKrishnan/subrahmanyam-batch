753b5915204ef4501d2cb2e2ad8ec7b6773d6379
Mintu Krishnan S <mintu.krish999@hotmail.com>
2021-04-23 13:02:16 +0530

    added files

--- node/middlewares/index.js
+++ node/middlewares/index.js
@@ -1,16 +1,56 @@
 const express = require('express');
 const app = express();
+const chalkAnimation = require('chalk-animation');
+
+// chalkAnimation.rainbow('Lorem ipsum dolor sit amet');
+
+let users = [
+  {
+    id: 1,
+    username: 'Mintu',
+    age: 25,
+  },
+  {
+    id: 2,
+    username: 'debasis',
+    age: 20,
+  },
+  {
+    id: 2,
+    username: 'sadaf',
+    age: 23,
+  },
+];
 
 const newMiddleware = (req, res, next) => {
-  console.log('i am a middleware');
+  chalkAnimation.rainbow('i am a middleware');
+
+  // console.log(chalkAnimation.rainbow('i am a middleware'));
   next();
 };
 
-app.get('/about', newMiddleware, (request, response) => {
-  response.send('about');
+app.get('/users', newMiddleware, (request, response) => {
+  // console.log(request.query.username);
+
+  // users.forEach((el) => {
+  //   console.log(el.username);
+  // });
+
+  // const new_user = users.map((el) => {
+  //   return el.username;
+  // });
+  // console.log(new_user);
+
+  let newUser = [...users];
+  if (request.query.username) {
+    newUser = users.filter((el) => {
+      return el.username == request.query.username;
+    });
+  }
+  response.send(newUser);
 });
 
-app.get('/home', newMiddleware, (request, response) => {
+app.get('/', newMiddleware, (request, response) => {
   response.send('home');
 });
 
